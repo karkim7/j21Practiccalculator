13e5978ca07f3f7511d39157f8d2e250afe25916
Mahesh Karki <mahesh.karkiyes45@gmail.com>
2025-06-21 09:20:48 -0400

    completed
    final result succeeded ok calculator

--- Program.cs
+++ Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to my switch practice session for new calculator!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("\n--- Simple Calculator ---");
+            Console.WriteLine("1. Add");
+            Console.WriteLine("2. Subtract");
+            Console.WriteLine("3. Multiply");
+            Console.WriteLine("4. Divide");
+            Console.WriteLine("5. Exit");
+            Console.Write("Choose an option (1-5): ");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            if (choice == 5)
+            {
+                running = false;
+                Console.WriteLine("Goodbye!");
+                break;
+            }
+
+            Console.Write("Enter first number: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double result = 0;
+
+            switch (choice)
+            {
+                case 1:
+                    result = num1 + num2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+                case 2:
+                    result = num1 - num2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+                case 3:
+                    result = num1 * num2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+                case 4:
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine("Result: " + result);
+                    }
+                    else
+                        Console.WriteLine("Error: Cannot divide by zero!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice!");
+                    break;
+            }
+        }
+    }
+}
--- obj/Debug/net9.0/j21calculator.AssemblyInfo.cs
+++ obj/Debug/net9.0/j21calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("j21calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cea83262cae7d9201abf3506f5f07a28174d52d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14c6b7fe6cd2c7c47082032320497a96bbadc595")]
 [assembly: System.Reflection.AssemblyProductAttribute("j21calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("j21calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
